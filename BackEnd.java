836628f07f8c58054774a948111a973c5bba0b61
ReiZedlav <150660845+ReiZedlav@users.noreply.github.com>
2024-04-15 19:00:24 +0800

    Update BackEnd.java

--- BackEnd.java
+++ BackEnd.java
@@ -1,1 +1,30 @@
 
+public class BackEnd{
+    double GPA;
+    String course;
+    String name;
+    String age;
+    
+    //PUT STUDENT RELATED DATA HERE
+}
+
+class Credentials extends Information{
+    String username;
+    String password;
+
+    Boolean authenticated = false;
+
+    public Credentials(String u, String p){ //CONSTRUCTOR IS USED FOR THE SIGNING UP PROCESS
+        this.username = u;
+        this.username = p;
+    }
+
+    public Boolean Authenticate(String u, String p){
+        if (this.username.equals(username) && this.password.equals(password)){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+}  
